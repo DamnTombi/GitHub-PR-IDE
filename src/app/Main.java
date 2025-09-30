e7f570398b080e5cb4310bd555465a0f662127cb
Nico <dfp098@gmail.com>
2025-09-30 22:33:50 +0300

    add substraction

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,6 +8,8 @@ public class Main {
         int b = 46;
         int c =add(a, b);
         int d = multiple (a,b);
+        double e = subctract (a,b);
+        System.out.println("Substract :"+a+"/"+b+"="+e);
         System.out.println("Multiple :"+a+"*"+b+"="+d);
         System.out.println("Add :"+a+"+"+b+"="+c);
 
@@ -17,5 +19,8 @@ public class Main {
     }
     private static int multiple(int a, int b){
         return a*b;
+    }
+    private static double subctract(double a, double b){
+        return a/b;
     }
 }
